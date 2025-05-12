690759ac833ed8a382bcb5ae623ad9f33578dd9c
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-05-12 22:58:45 +0530

    Updated tests for  GeometryData

--- src/test/java/com/inrix/model/GeometryDataTest.java
+++ src/test/java/com/inrix/model/GeometryDataTest.java
@@ -15,10 +15,18 @@ class GeometryDataTest {
         GeometryData geometryData = new GeometryData();
         List<List<List<Double>>> coordinates = new ArrayList<>();
 
+        // Create a nested list structure with at least one element
+        List<List<Double>> outerList = new ArrayList<>();
+        List<Double> innerList = new ArrayList<>();
+        innerList.add(1.0);
+        innerList.add(2.0);
+        outerList.add(innerList);
+        coordinates.add(outerList);
+
         // Act
         geometryData.setCoordinates(coordinates);
 
         // Assert
-        assertEquals(coordinates, geometryData.getCoordinates());
+        assertEquals(outerList, geometryData.getCoordinates());
     }
 }
