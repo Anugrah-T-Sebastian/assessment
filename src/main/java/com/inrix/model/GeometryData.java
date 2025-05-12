c0453999f6d5d426fec0239d6f1475b24f695587
Anugrah Sebastian <87081811+Anugrah-T-Sebastian@users.noreply.github.com>
2025-05-12 12:34:46 +0530

    Refactoring: Removed redundant code from GeometryData

--- src/main/java/com/inrix/model/GeometryData.java
+++ src/main/java/com/inrix/model/GeometryData.java
@@ -1,40 +1,11 @@
 package com.inrix.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import org.locationtech.jts.geom.Coordinate;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class GeometryData {
     private String type;
     private List<List<List<Double>>> coordinates;
 
-//    @JsonIgnore
-//    public List<Coordinate> getLineCoordinates() {
-//        if ("LineString".equals(type)) {
-//            List<List<Double>> coords = (List<List<Double>>) coordinates;
-//            return coords.stream()
-//                    .map(coord -> new Coordinate(coord.get(0), coord.get(1)))
-//                    .collect(Collectors.toList());
-//        }
-//        throw new IllegalStateException("Not a LineString geometry");
-//    }
-
-//    @JsonIgnore
-//    public List<Coordinate> getPolygonCoordinates() {
-//        if ("Polygon".equals(type)) {
-//            List<List<List<Double>>> rings = (List<List<List<Double>>>) coordinates;
-//            // Get first ring (outer boundary)
-//            return rings.get(0).stream()
-//                    .map(coord -> new Coordinate(coord.get(0), coord.get(1)))
-//                    .collect(Collectors.toList());
-//        }
-//        throw new IllegalStateException("Not a Polygon geometry");
-//    }
-
-
-
-    // Standard getters
     public String getType() { return type; }
 
     public List<List<Double>> getCoordinates() {
